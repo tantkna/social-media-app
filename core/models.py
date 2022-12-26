1c82243cd2e8c30da8f3054b3a015e022a2bac1a
tantkna <tan10121999@gmail.com>
2022-12-27 06:55:09 +0700

    Add like post

--- core/models.py
+++ core/models.py
@@ -23,3 +23,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
 
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.username
+    
+
+
